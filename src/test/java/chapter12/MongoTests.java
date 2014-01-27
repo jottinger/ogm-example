073cb0812389bc2d4a1b896f3aaec3649ead53b5
jottinger <joeo@enigmastation.com>
2014-01-27 11:16:05 -0500

    initial project commit, fails mongo CR

--- src/test/java/chapter12/MongoTests.java
+++ src/test/java/chapter12/MongoTests.java
@@ -6,13 +6,16 @@ import javax.persistence.EntityManager;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
 public class MongoTests extends BaseOGMTests {
     @Test
     public void testCR() {
         EntityManager em = getEntityManager();
+        em.getTransaction().begin();
         Person person = new Person("user 1", 1);
         em.persist(person);
+        em.getTransaction().commit();
         em.close();
         System.out.println(person);
         em=getEntityManager();
@@ -21,6 +24,56 @@ public class MongoTests extends BaseOGMTests {
         System.out.println(p2);
         assertNotNull(p2);
         assertEquals(p2, person);
+    }
+
+    @Test
+    public void testUpdate() {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Person person = new Person("user 2", 1);
+        em.persist(person);
+        em.getTransaction().commit();
+        em.close();
+
+        em=getEntityManager();
+        em.getTransaction().begin();
+        Person p2=em.find(Person.class, person.getId());
+        p2.setBalance(2);
+        em.getTransaction().commit();
+        em.close();
+
+        em=getEntityManager();
+        em.getTransaction().begin();
+        Person p3=em.find(Person.class, person.getId());
+        em.getTransaction().commit();
+        em.close();
+
+        assertEquals(p3, p2);
+    }
+
+    @Test
+    public void testDelete() {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Person person = new Person("user 3", 1);
+        em.persist(person);
+        em.getTransaction().commit();
+        em.close();
+
+        em=getEntityManager();
+        em.getTransaction().begin();
+        Person p2=em.find(Person.class, person.getId());
+        em.remove(p2);
+        em.getTransaction().commit();
+        em.close();
+
+        em=getEntityManager();
+        em.getTransaction().begin();
+        Person p3=em.find(Person.class, person.getId());
+        em.getTransaction().commit();
+        em.close();
+
+        assertNull(p3);
     }
 
     @Override
